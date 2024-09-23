a7222da7e89ed0f233be62016929b6dc2fb63624
minjjjun <163504482+minjjjun@users.noreply.github.com>
2024-09-23 18:33:50 +0900

    Create 240920.py

---
+++ 240920.py
@@ -0,0 +1,20 @@
+#1
+a="Hello, World!"
+print(a[0:5])
+
+#2
+result=(7+3)**2
+print(result)
+
+#3
+A=[1,2,3,4,5]
+A.append(4)
+A.append(3)
+A.append(2)
+A.append(1)
+result = A[4:9]
+print(result)
+
+#4
+fruit={"apple":3, "banana":5, "cherry":2}
+print(fruit["banana"])
